156b89117e8251d2f32f4b81a6c47af55c462271
Harrison Anthony <harrisoneanthony@gmail.com>
2023-03-29 20:05:16 -0400

    tribonacci

--- algorithm_practice/tribonacci.py
+++ algorithm_practice/tribonacci.py
@@ -25,6 +25,11 @@ def tribonacci(signature, n):
         i+=1
     return results
 
+# def tribonacci(signature, n):
+    # res = signature[:n]
+    # for i in range(n - 3): res.append(sum(res[-3:]))
+    # return res
+
 print(tribonacci([1,1,1],10))
 print(tribonacci([1,1,1],1))
 print(tribonacci([300,200,100],0))
