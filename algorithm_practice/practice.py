0acbf01e2bc04c0b532181ea1e83101a70bed9b4
Harrison Anthony <harrisoneanthony@gmail.com>
2023-04-25 13:58:13 -0400

    practice notes

--- algorithm_practice/practice.py
+++ algorithm_practice/practice.py
@@ -49,7 +49,7 @@ class B(A):
     def method(self):
         print(2)
 
-B().method()
+# B().method()
 
 #magic methods
 '''
@@ -71,3 +71,71 @@ __xor__ for ^
 
 __or__ for |
 '''
+
+'''
+Python also provides magic methods for comparisons.
+
+__lt__ for <
+
+__le__ for <=
+
+__eq__ for ==
+
+__ne__ for !=
+
+__gt__ for >
+
+__ge__ for >=
+'''
+
+'''
+There are several magic methods for making classes act like containers.
+
+__len__ for len()
+
+__getitem__ for indexing
+
+__setitem__ for assigning to indexed values
+
+__delitem__ for deleting indexed values
+
+__iter__ for iteration over objects (e.g., in for loops)
+
+__contains__ for in
+'''
+
+'''class Test:
+    __egg = 7
+
+    def get_egg(self):
+        return Test.__egg
+
+t = Test()
+print(t.get_egg())
+
+-OR-
+
+class Test:
+    __egg = 7
+
+t = Test()
+print(t._Test__egg)'''
+
+
+'''
+Exceptions 
+Different exceptions are raised for different reasons. 
+
+Common exceptions:
+
+ImportError: an import fails;
+
+IndexError: a list is indexed with an out-of-range number;
+
+NameError: an unknown variable is used;
+
+SyntaxError: the code can't be parsed properly; 
+
+TypeError: a function is called on a value of an inappropriate type;
+
+ValueError: a function is called on a value of the correct type, but with an inappropriate value.'''
